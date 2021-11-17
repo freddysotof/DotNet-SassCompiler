4cb732d4f8b51b06d274a78cdb5844f7d0d1ff4e
Freddy Soto <45477077+freddysotof@users.noreply.github.com>
2021-11-16 21:06:28 -0400

    Add files via upload

---
+++ Gruntfile.js
@@ -0,0 +1,35 @@
+﻿/// <binding AfterBuild='default' />
+module.exports = function (grunt) {
+    'use strict';
+    const sass = require('node-sass');
+    // Project configuration.
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+
+        // Sass
+        sass: {
+            options: {
+                implementation:sass,
+                sourceMap: true, // Create source map
+                outputStyle: 'compressed' // Minify output
+            },
+            dist: {
+                files: [
+                    {
+                        expand: true, // Recursive
+                        cwd: "Styles", // The startup directory
+                        src: ["**/*.scss"], // Source files
+                        dest: "wwwroot/css/pm", // Destination
+                        ext: ".css" // File extension
+                    }
+                ]
+            }
+        }
+    });
+
+    // Load the plugin
+    grunt.loadNpmTasks('grunt-sass');
+    //grunt.loadNpmTasks('grunt-contrib-sass');
+    // Default task(s).
+    grunt.registerTask('default', ['sass']);
+};
